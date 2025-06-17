0c005cc2f2560f43b0ec25477045694501c4a2f9
Hugo Santos <hugo.santos@factorlibre.com>
2025-06-17 09:34:36 +0200

    Revert "[IMP] product_harmonized_system: Remove company_id"
    This reverts commit 6f5d47735e74fdbf68729fedc67194465cc6a81c.

--- product_harmonized_system/migrations/16.0.2.0.0/pre-migration.py
+++
@@ -1,9 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.rename_columns(env.cr, {"hs_code": [("company_id", None)]})
-    rule = env.ref("product_harmonized_system.hs_code_company_rule")
-    if rule:
-        rule.unlink()
